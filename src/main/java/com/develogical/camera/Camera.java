550f8b0321561f8e73abe0693d2527e6f09bddf8
Adriano Reis <reis.a@outlook.com>
2017-07-11 20:55:12 +0100

    Exercise item 1

--- src/main/java/com/develogical/camera/Camera.java
+++ src/main/java/com/develogical/camera/Camera.java
@@ -1,13 +1,13 @@
 package com.develogical.camera;
 
 public class Camera {
-
+    private Sensor sensor;
     public void pressShutter() {
         // not implemented
     }
 
     public void powerOn() {
-        // not implemented
+        sensor.powerUp();
     }
 
     public void powerOff() {
--- src/test/java/com/develogical/camera/CameraTest.java
+++ src/test/java/com/develogical/camera/CameraTest.java
@@ -1,13 +1,24 @@
 package com.develogical.camera;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.mockito.Mockito.*;
 
+@RunWith(MockitoJUnitRunner.class)
 public class CameraTest {
+    @InjectMocks
+    private Camera camera;
+    @Mock
+    private Sensor sensor;
+
     @Test
     public void switchingTheCameraOnPowersUpTheSensor() {
-        // write your test here
+        camera.powerOn();
+        verify(sensor).powerUp();
     }
 }
